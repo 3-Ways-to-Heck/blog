7eea8ea6e8d86aa1f5a9aa980624969726d1b806
Katrina Lee <katrinawlee@gmail.com>
2021-04-02 17:43:56 -0700

    fixed variable names to match those of the encryption source code

--- 2021/SPR/picoCTF-2021/cryptography/New-Caesar/new_caesar_solution.py
+++ 2021/SPR/picoCTF-2021/cryptography/New-Caesar/new_caesar_solution.py
@@ -1,34 +1,33 @@
 import base64
 
 given = "apbopjbobpnjpjnmnnnmnlnbamnpnononpnaaaamnlnkapndnkncamnpapncnbannaapncndnlnpna"
+ALPHABET = list("abcdefghijklmnop")
 
-alpha = list("abcdefghijklmnop")
-
-def b16_d(ciph):
+def b16_decode(ciph):
 	plain = ""
 	ciph = list(ciph)
 	enc = [ciph[i * 2:(i + 1) * 2] for i in range((len(ciph) + 2 - 1) // 2)]
 	for i in enc:
-		bin1 = "{0:04b}".format(alpha.index(i[0]))
-		bin2 = "{0:04b}".format(alpha.index(i[1]))
+		bin1 = "{0:04b}".format(ALPHABET.index(i[0]))
+		bin2 = "{0:04b}".format(ALPHABET.index(i[1]))
 		binary = bin1 + bin2
 		c = chr(int(binary, 2))
 		plain += c
 	return plain
 
-all_flags = []
+possible_flags = []
 
-for key in alpha:
-	unshifted = ""
+for key in ALPHABET:
+	enc = ""
 
 	for i in given:
-		c = alpha.index(i) + 194 - ord(key)
+		c = ALPHABET.index(i) + 194 - ord(key)
 		f = 1
 		while c not in range(97, 113):
-			c = alpha.index(i) + 194 - ord(key) + 16 * f
+			c = ALPHABET.index(i) + 194 - ord(key) + 16 * f
 			f += 1
-		unshifted += chr(c)
+		enc += chr(c)
 
-	all_flags.append(b16_d(unshifted))
+	possible_flags.append(b16_decode(enc))
 
-print("\n".join(all_flags))
+print("\n".join(possible_flags))
